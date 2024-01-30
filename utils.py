50ab737d9c05219b12f0b9944f3c09e020c1fca4
Masahiko Funaki <mfunaki@gmail.com>
2024-01-31 00:42:15 +0900

    Add get_google_api_key

--- utils.py
+++ utils.py
@@ -123,6 +123,10 @@ def get_openai_api_key():
     openai_api_key = os.getenv("OPENAI_API_KEY")
     return openai_api_key
 
+def get_google_api_key():
+    google_api_key = os.getenv("GOOGLE_API_KEY")
+    return google_api_key
+
 def get_circle_api_key():
     circle_token = os.getenv("CIRCLE_TOKEN")
     return circle_token
